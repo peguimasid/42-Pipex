990316eebb766f04c65be738eeb19189e4a0349a
Guilhermo Masid <guilhermomasid@gmail.com>
2022-07-11 15:00:00 -0300

    example of using pipe with processes

--- pipex.c
+++ pipex.c
@@ -6,26 +6,47 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 10:59:07 by gmasid            #+#    #+#             */
-/*   Updated: 2022/07/11 12:35:35 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/07/11 14:59:59 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
 
-void	print_usage(void)
+int	main(void)
 {
-	ft_putstr_fd("Please provider all arguments:\n", 1);
-	ft_putstr_fd("./pipex INFILE CMD1 CMD2 OUTFILE\n", 1);
-}
+	int	fd[2];
+	int	id;
+	int	x;
+	int	y;
 
-int	main(int argc, char *argv[])
-{
-	if (argc != 5)
+	// fd[0] - read
+	// fd[1] - write
+	pipe(fd);
+	id = fork();
+	// we're in the child process
+	if (id == 0)
 	{
-		print_usage();
-		return (1);
+		// close because we not gonna read, just write
+		close(fd[0]);
+		printf("input a number: ");
+		scanf("%d", &x);
+		// set fd[1] to be x
+		write(fd[1], &x, sizeof(int));
+		// at this point, we close write end and the parent process
+		// has access to that data we write in his read end (fd[0])
+		close(fd[1]);
 	}
-	printf("%d\n", argc);
-	printf("%s %s %s %s", argv[1], argv[2], argv[3], argv[4]);
+	// we're in the parent process
+	else
+	{
+		// close because we not gonna write data, just read
+		close(fd[1]);
+		// read the value that child process got
+		read(fd[0], &y, sizeof(int));
+		// close the read end
+		close(fd[0]);
+		// print that value
+		printf("got from child process: %d", y);
+	}
 }
---
+++ run_program.sh
@@ -0,0 +1,1 @@
+make -s && ./pipex infile.txt "cat" "grep 42" outfile.txt && make -s fclean
